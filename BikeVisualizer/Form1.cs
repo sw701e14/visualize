cc9867770be5b9b4f62185ab384c7d925a9c2b20
deaddog <mikkelslsl@gmail.com>
2014-12-16 12:42:42 +0100

    Initial commit with all the 'goodies'...

---
+++ BikeVisualizer/Form1.cs
@@ -0,0 +1,204 @@
+﻿using Shared.DAL;
+using Shared.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BikeVisualizer
+{
+    public partial class Form1 : Form
+    {
+        private const float zoom = 1.2f;
+
+        private System.Drawing.Drawing2D.Matrix transform = new System.Drawing.Drawing2D.Matrix();
+
+        private Image mapImage;
+
+        private PointF[] locations;
+        private PointF[] hotspotLocations;
+        private PointF[] hotspotLocations2;
+        private PointF[][] hotspots;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            SetStyle(
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.ResizeRedraw |
+                ControlStyles.UserPaint,
+                true);
+
+            foreach (Control c in this.Controls)
+                c.MouseWheel += (s, e) => OnMouseWheel(e);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            mapImage = Image.FromFile("map.png");
+            transform.Translate((this.Width - 1400) / 2, (this.Height - 700) / 2);
+
+            //BackgroundWorker w = new BackgroundWorker();
+            //w.DoWork += (s, ee) => updateClusters();
+            //w.RunWorkerAsync();
+            //updateClusters();
+            timer1.Start();
+        }
+
+        private void updateClusters()
+        {
+            timer1.Stop();
+            GPSLocation[] loc;
+            GPSLocation[] hot;
+
+            using (Database db = new Database())
+            {
+                loc = db.RunSession(s => GPSData.GetAll(s).Select(x => x.Location).ToArray());
+                hot = db.RunSession(s => GPSData.GetAllHasNotMoved(s).Select(x => x.Location).ToArray());
+                hotspots = db.RunSession(s => Hotspot.LoadAllHotspots(s).Select(x => x.getDataPoints().ToPixels()).ToArray());
+
+                var m = db.RunSession(s => MarkovChain.LoadMarkovChain(s));
+            }
+
+            locations = loc.ToPixels();
+            hotspotLocations = hot.ToPixels();
+
+            this.Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            e.Graphics.Transform = transform;
+
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+            if (locations != null)
+                e.Graphics.FillPoints(Brushes.Purple, locations, 0.25f);
+
+            if (hotspotLocations != null)
+                e.Graphics.FillPoints(Brushes.Blue, hotspotLocations, 0.25f);
+
+            if (hotspots != null)
+                using (SolidBrush brush = new SolidBrush(Color.FromArgb(120, Color.Red)))
+                using (Pen pen = new Pen(Color.Red, 0.25f) { LineJoin = System.Drawing.Drawing2D.LineJoin.Round })
+                    foreach (var h in hotspots)
+                    {
+                        e.Graphics.FillPolygon(brush, h);
+                        e.Graphics.DrawPolygon(pen, h);
+                    }
+
+            if (hotspotLocations2 != null)
+                e.Graphics.FillPoints(Brushes.Green, hotspotLocations2, 0.125f);
+
+            PointF[] p = new PointF[] { mouse };
+            var m = e.Graphics.Transform;
+            m.Invert();
+            m.TransformPoints(p);
+
+            GPSLocation gps = p[0].ToGPS();
+
+            //e.Graphics.FillPoint(Brushes.Pink, p[0], 4);
+            using (Font f = new Font(this.Font.FontFamily, this.Font.Size * 0.05f))
+                e.Graphics.DrawString(gps.ToString(), f, Brushes.Black, p[0]);
+
+            //GPSLocation loc = new GPSLocation(57.06m, 9.84m);
+            //e.Graphics.FillPoint(Brushes.Red, loc);
+        }
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            base.OnPaintBackground(e);
+
+            e.Graphics.Transform = transform;
+
+            if (mapImage != null)
+                e.Graphics.DrawImage(mapImage, new Rectangle(0, 0, 1400, 700));
+        }
+
+        private Point mouse = new Point();
+        private bool moving = false;
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                moving = true;
+                mouse = e.Location;
+            }
+        }
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                moving = false;
+        }
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseWheel(e);
+            if (moving)
+            {
+                transform.Translate(e.X - mouse.X, e.Y - mouse.Y, System.Drawing.Drawing2D.MatrixOrder.Append);
+            }
+            mouse = e.Location;
+            this.Invalidate();
+        }
+
+        protected override void OnMouseWheel(MouseEventArgs e)
+        {
+            base.OnMouseWheel(e);
+            if (e.Delta > 0)
+                doZoom(zoom);
+            else
+                doZoom(1f / zoom);
+        }
+
+        protected override void OnDoubleClick(EventArgs e)
+        {
+            base.OnDoubleClick(e);
+            if (Control.ModifierKeys == Keys.Control)
+                doZoom(1f / zoom);
+            else
+                doZoom(zoom);
+        }
+
+        private void doZoom(float factor)
+        {
+            transform.Translate(-mouse.X, -mouse.Y, System.Drawing.Drawing2D.MatrixOrder.Append);
+            transform.Scale(factor, factor, System.Drawing.Drawing2D.MatrixOrder.Append);
+            transform.Translate(mouse.X, mouse.Y, System.Drawing.Drawing2D.MatrixOrder.Append);
+            this.Invalidate();
+        }
+
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            this.Text = "Neighbours: " + trackBar1.Value + " | Distance: " + trackBar2.Value;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            this.Text = "Neighbours: " + trackBar1.Value + " | Distance: " + trackBar2.Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            updateClusters();
+            Invalidate();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            updateClusters();
+        }
+    }
+}
---
+++ BikeVisualizer/GraphicsExtension.cs
@@ -0,0 +1,108 @@
+﻿using Shared.DTO;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeVisualizer
+{
+    public static class GraphicsExtension
+    {
+        private static double getLatPixels(double lat)
+        {
+            double x = 57.005 - 57.06;
+            double y = 657 - 69;
+            double a = y / x;
+
+            return a * (lat - 57.005) + 657;
+        }
+        private static double getLngPixels(double lng)
+        {
+            double x = 10.02 - 9.84;
+            double y = 1237 - 188;
+            double a = y / x;
+
+            return a * (lng - 10.02) + 1237;
+        }
+        private static double getLatGPS(double lat)
+        {
+            double x = 657 - 69;
+            double y = 57.005 - 57.06;
+            double a = y / x;
+
+            return a * (lat - 657) + 57.005;
+        }
+        private static double getLngGPS(double lng)
+        {
+            double x = 1237 - 188;
+            double y = 10.02 - 9.84;
+            double a = y / x;
+
+            return a * (lng - 1237) + 10.02;
+        }
+        public static PointF ToPixels(this GPSLocation location)
+        {
+            return new PointF((float)getLngPixels((double)location.Longitude), (float)getLatPixels((double)location.Latitude));
+        }
+        public static PointF ToPixels(this GPSData data)
+        {
+            return new PointF((float)getLngPixels((double)data.Location.Longitude), (float)getLatPixels((double)data.Location.Latitude));
+        }
+        public static PointF[] ToPixels(this GPSLocation[] locations)
+        {
+            PointF[] points = new PointF[locations.Length];
+            for (int i = 0; i < points.Length; i++)
+                points[i] = ToPixels(locations[i]);
+            return points;
+        }
+        public static PointF[] ToPixels(this GPSData[] data)
+        {
+            PointF[] points = new PointF[data.Length];
+            for (int i = 0; i < points.Length; i++)
+                points[i] = ToPixels(data[i].Location);
+            return points;
+        }
+
+        public static GPSLocation ToGPS(this PointF point)
+        {
+            return new GPSLocation((decimal)getLatGPS(point.Y), (decimal)getLngGPS(point.X));
+        }
+
+        public static void FillPoint(this Graphics graphics, Brush brush, GPSLocation location, float radius = 1.5f)
+        {
+            var p = ToPixels(location);
+            graphics.FillEllipse(brush, p.X - radius, p.Y - radius, radius * 2, radius * 2);
+        }
+        public static void FillPoints(this Graphics graphics, Brush brush, GPSLocation[] locations, float radius = 1.5f)
+        {
+            foreach (var p in locations)
+                FillPoint(graphics, brush, p, radius);
+        }
+
+        public static void FillPoint(this Graphics graphics, Brush brush, PointF p, float radius = 1.5f)
+        {
+            graphics.FillEllipse(brush, p.X - radius, p.Y - radius, radius * 2, radius * 2);
+        }
+        public static void FillPoints(this Graphics graphics, Brush brush, PointF[] locations, float radius = 1.5f)
+        {
+            foreach (var p in locations)
+                FillPoint(graphics, brush, p, radius);
+        }
+
+        public static void FillPolygon(this Graphics graphics, Brush brush, GPSLocation[] locations)
+        {
+            graphics.FillPolygon(brush, ToPixels(locations));
+        }
+        public static void DrawPolygon(this Graphics graphics, Pen pen, GPSLocation[] locations)
+        {
+            graphics.DrawPolygon(pen, ToPixels(locations));
+        }
+
+        public static void DrawLines(this Graphics graphics, Pen pen, GPSLocation[] locations)
+        {
+            graphics.DrawLines(pen, ToPixels(locations));
+        }
+    }
+}
