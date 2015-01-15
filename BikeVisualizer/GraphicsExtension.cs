9cf5d034f23a474447223d8e903cb3b674722470
deaddog <mikkelslsl@gmail.com>
2015-01-15 16:24:58 +0100

    Removed all paint methods that operated directly on GPSLocation elements.

--- BikeVisualizer/GraphicsExtension.cs
+++ BikeVisualizer/GraphicsExtension.cs
@@ -45,11 +45,12 @@ namespace BikeVisualizer
 
             return a * (lng - pixel2.X) + (double)gps2.Longitude;
         }
-        public static PointF ToPixels(this GPSLocation location)
+
+        public static PointF ToPixel(this GPSLocation location)
         {
             return new PointF((float)getLngPixels((double)location.Longitude), (float)getLatPixels((double)location.Latitude));
         }
-        public static PointF ToPixels(this GPSData data)
+        public static PointF ToPixel(this GPSData data)
         {
             return new PointF((float)getLngPixels((double)data.Location.Longitude), (float)getLatPixels((double)data.Location.Latitude));
         }
@@ -57,14 +58,14 @@ namespace BikeVisualizer
         {
             PointF[] points = new PointF[locations.Length];
             for (int i = 0; i < points.Length; i++)
-                points[i] = ToPixels(locations[i]);
+                points[i] = ToPixel(locations[i]);
             return points;
         }
         public static PointF[] ToPixels(this GPSData[] data)
         {
             PointF[] points = new PointF[data.Length];
             for (int i = 0; i < points.Length; i++)
-                points[i] = ToPixels(data[i].Location);
+                points[i] = ToPixel(data[i].Location);
             return points;
         }
 
@@ -73,36 +74,15 @@ namespace BikeVisualizer
             return new GPSLocation((decimal)getLatGPS(point.Y), (decimal)getLngGPS(point.X));
         }
 
-        public static void FillPoint(this Graphics graphics, Brush brush, GPSLocation location, float radius = 1.5f)
-        {
-            var p = ToPixels(location);
-            graphics.FillEllipse(brush, p.X - radius, p.Y - radius, radius * 2, radius * 2);
-        }
 
         public static void FillPoint(this Graphics graphics, Brush brush, PointF location, float radius = 1.5f)
         {
             graphics.FillEllipse(brush, location.X - radius, location.Y - radius, radius * 2, radius * 2);
         }
-
-        public static void FillPoints(this Graphics graphics, Brush brush, GPSLocation[] locations, float radius = 1.5f)
-        {
-            foreach (var p in locations)
-                FillPoint(graphics, brush, p, radius);
-        }
-
-        
         public static void FillPoints(this Graphics graphics, Brush brush, PointF[] locations, float radius = 1.5f)
         {
             foreach (var p in locations)
                 FillPoint(graphics, brush, p, radius);
-        }
-
-        public static void DrawCross(this Graphics graphics, Pen pen, GPSLocation loc, float radius = 1.5f)
-        {
-            var p = ToPixels(loc);
-
-            graphics.DrawLine(pen, new PointF(p.X - radius, p.Y), new PointF(p.X + radius, p.Y));
-            graphics.DrawLine(pen, new PointF(p.X, p.Y - radius), new PointF(p.X, p.Y + radius));
         }
 
         public static void DrawCross(this Graphics graphics, Pen pen, PointF p, float radius = 1.5f)
@@ -110,33 +90,12 @@ namespace BikeVisualizer
             graphics.DrawLine(pen, new PointF(p.X - radius, p.Y), new PointF(p.X + radius, p.Y));
             graphics.DrawLine(pen, new PointF(p.X, p.Y - radius), new PointF(p.X, p.Y + radius));
         }
-
-        public static void DrawCrosses(this Graphics graphics, Pen pen, GPSLocation[] locations, float radius = 1.5f)
-        {
-            foreach (var p in locations)
-            {
-                DrawCross(graphics, pen, p, radius);
-            }
-        }
-
         public static void DrawCrosses(this Graphics graphics, Pen pen, PointF[] locations, float radius = 1.5f)
         {
             foreach (var p in locations)
             {
                 DrawCross(graphics, pen, p, radius);
             }
-        }
-
-
-        public static void DrawPoint(this Graphics graphics, Pen brush, GPSLocation location, float radius = 1.5f)
-        {
-            var p = ToPixels(location);
-            graphics.DrawEllipse(brush, p.X - radius, p.Y - radius, radius * 2, radius * 2);
-        }
-        public static void DrawPoints(this Graphics graphics, Pen brush, GPSLocation[] locations, float radius = 1.5f)
-        {
-            foreach (var p in locations)
-                DrawPoint(graphics, brush, p, radius);
         }
 
         public static void DrawPoint(this Graphics graphics, Pen brush, PointF p, float radius = 1.5f)
@@ -153,27 +112,12 @@ namespace BikeVisualizer
         {
             graphics.FillRectangle(brush, p.X - radius, p.Y - radius, radius * 2, radius * 2);
         }
-
         public static void FillRectangles(this Graphics graphics, Brush brush, PointF[] locations, float radius = 1.5f)
         {
             foreach (var p in locations)
             {
                 graphics.FillRectangle(brush, p, radius);
             }
-        }
-
-        public static void FillPolygon(this Graphics graphics, Brush brush, GPSLocation[] locations)
-        {
-            graphics.FillPolygon(brush, ToPixels(locations));
-        }
-        public static void DrawPolygon(this Graphics graphics, Pen pen, GPSLocation[] locations)
-        {
-            graphics.DrawPolygon(pen, ToPixels(locations));
-        }
-
-        public static void DrawLines(this Graphics graphics, Pen pen, GPSLocation[] locations)
-        {
-            graphics.DrawLines(pen, ToPixels(locations));
         }
     }
 }
