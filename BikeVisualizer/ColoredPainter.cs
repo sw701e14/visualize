3a5466d1cbbeb84239a54dca229bbb7ce617285f
deaddog <mikkelslsl@gmail.com>
2015-01-15 17:03:20 +0100

    Created an abstract class for the various painter types.

---
+++ BikeVisualizer/ColoredPainter.cs
@@ -0,0 +1,29 @@
+﻿using Shared.DAL;
+using System.Drawing;
+
+namespace BikeVisualizer
+{
+    public abstract class ColoredPainter : IPainter, IGPSConsumer
+    {
+        private Color color;
+
+        public ColoredPainter(Color color)
+        {
+            this.color = color;
+        }
+
+        public Color Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public abstract void Load(DatabaseSession session);
+
+        public void Paint(Graphics graphics)
+        {
+            Paint(graphics, 1f / graphics.Transform.Elements[0]);
+        }
+        public abstract void Paint(Graphics graphics, float widthScale);
+    }
+}
