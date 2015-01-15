69527c76496ea0e5f99b24e40dbc423ac967f3c8
deaddog <mikkelslsl@gmail.com>
2015-01-15 13:15:06 +0100

    Copied the zoom-handling code from Form1 to DisplayForm.

--- BikeVisualizer/DisplayForm.cs
+++ BikeVisualizer/DisplayForm.cs
@@ -14,6 +14,7 @@ namespace BikeVisualizer
 {
     public partial class DisplayForm : Form
     {
+        private const float zoom = 1.2f;
         private static readonly Size GoogleMapsSize = new Size(640, 640);
         private static readonly GPSLocation InitialCenter = new GPSLocation(57.0325m, 9.93m);
 
@@ -62,6 +63,33 @@ namespace BikeVisualizer
             if (moving)
                 transform.Translate(e.X - mouse.X, e.Y - mouse.Y, Draw.MatrixOrder.Append);
             mouse = e.Location;
+            this.Invalidate();
+        }
+
+        protected override void OnMouseWheel(MouseEventArgs e)
+        {
+            base.OnMouseWheel(e);
+            if (e.Delta > 0)
+                doZoom(zoom);
+            else
+                doZoom(1f / zoom);
+        }
+
+        protected override void OnDoubleClick(EventArgs e)
+        {
+            base.OnDoubleClick(e);
+            if (Control.ModifierKeys == Keys.Control)
+                doZoom(1f / zoom);
+            else
+                doZoom(zoom);
+        }
+
+        private void doZoom(float factor)
+        {
+            transform.Translate(-mouse.X, -mouse.Y, System.Drawing.Drawing2D.MatrixOrder.Append);
+            transform.Scale(factor, factor, System.Drawing.Drawing2D.MatrixOrder.Append);
+            transform.Translate(mouse.X, mouse.Y, System.Drawing.Drawing2D.MatrixOrder.Append);
+
             this.Invalidate();
         }
     }
