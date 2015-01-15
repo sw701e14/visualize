0316c1a17b1287160a196f37c3b23d342e68a88b
deaddog <mikkelslsl@gmail.com>
2015-01-15 15:39:34 +0100

    Created a MatrixEntension class that allows for translation of single points.

---
+++ BikeVisualizer/MatrixEntension.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace BikeVisualizer
+{
+    public static class MatrixEntension
+    {
+        public static PointF TransformPoint(this Matrix matrix, PointF point)
+        {
+            PointF[] array = new PointF[] { point };
+            matrix.TransformPoints(array);
+            return array[0];
+        }
+    }
+}
