40ae0498e6107d088d15bc93b1ef169eed24b817
deaddog <mikkelslsl@gmail.com>
2015-01-15 12:07:45 +0100

    Program loads the DisplayForm instead of Form1.

--- BikeVisualizer/Program.cs
+++ BikeVisualizer/Program.cs
@@ -16,7 +16,7 @@ namespace BikeVisualizer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DisplayForm());
         }
     }
 }
