c40e314c6ad779d811c1675dd90b1db59097b495
deaddog <mikkelslsl@gmail.com>
2015-01-16 08:43:08 +0100

    DisplayForm cannot be resized.

--- BikeVisualizer/DisplayForm.Designer.cs
+++ BikeVisualizer/DisplayForm.Designer.cs
@@ -42,10 +42,12 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "DisplayForm";
             this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "GPS plotting";
             this.ResumeLayout(false);
 
